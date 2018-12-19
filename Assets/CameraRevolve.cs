055cc46ec2ac2952e3735db3028a7781b78faa63
unknown <C15337401@mydit.ie>
2018-12-19 15:53:48 +0000

    Added revolving camera mechanic

---
+++ Assets/CameraRevolve.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRevolve : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+    public Camera cam4;
+    public Camera cam5;
+
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+        cam3.enabled = false;
+        cam4.enabled = false;
+        cam5.enabled = false;
+    }
+
+     void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && (cam1.enabled == true))
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+            cam3.enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && (cam2.enabled == true))
+        {
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && (cam3.enabled == true))
+        {
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = false;
+            cam4.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && (cam4.enabled == true))
+        {
+            cam1.enabled = false;
+            cam2.enabled = false;
+            cam3.enabled = false;
+            cam4.enabled = false;
+            cam5.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && (cam5.enabled == true))
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+            cam3.enabled = false;
+            cam4.enabled = false;
+            cam5.enabled = false;
+        }
+
+    }
+}
